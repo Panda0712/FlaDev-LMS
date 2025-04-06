7a5b0173a09bd63ee902b318ab489b223528e23b
Pham Tuan <tuan.pham.super@gmail.com>
2025-04-06 18:48:57 +0700

    contact ui

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,19 +1,13 @@
-import { ChevronRight } from "lucide-react";
 import { useLocation } from "react-router-dom";
 import Image404 from "~/assets/images/404.png";
+import NavigationText from "~/components/NavigationText/NavigationText";
 
 const NotFound = () => {
   const location = useLocation();
 
   return (
     <section>
-      <div className="h-[60px] bg-[#f5f5f5] pl-28 flex items-center gap-2">
-        <span className="text-[#555555] font-medium">Trang chủ</span>
-        <span>
-          <ChevronRight size={14} className="text-[#9d9d9d]" />
-        </span>
-        <span className="text-[#9d9d9d]">{location.pathname.slice(1)}</span>
-      </div>
+      <NavigationText placeTo={location.pathname.slice(1)} />
       <div class="animate-fadeIn py-16 px-28">
         <h2 className="text-[32px] font-semibold mb-6">
           Lỗi! Không tìm thấy trang!
