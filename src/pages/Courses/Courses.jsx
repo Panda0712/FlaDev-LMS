a5b264ca226775dffd45e6b5a4e02237bb7e99d3
Pham Tuan <tuan.pham.super@gmail.com>
2025-04-10 07:41:22 +0700

    initial courses page ui

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,5 +1,19 @@
+import NavigationText from "~/components/NavigationText/NavigationText";
+
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <section>
+      <NavigationText placeTo="Danh sách khóa học" />
+
+      <div className="flex gap-[32px] items-start justify-between">
+        <div>
+          <div className="flex items-center justify-between gap-5"></div>
+          <div></div>
+        </div>
+        <div className="flex flex-col gap-[32px]"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Courses;
