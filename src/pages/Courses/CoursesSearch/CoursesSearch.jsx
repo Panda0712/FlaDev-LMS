7c12d33193fbe18dbd9313335a98ecd380d65b01
Pham Tuan <tuan.pham.super@gmail.com>
2025-04-10 10:03:40 +0700

    handle refactor courses ui

---
+++ src/pages/Courses/CoursesSearch/CoursesSearch.jsx
@@ -0,0 +1,25 @@
+import { Search } from "lucide-react";
+import Input from "~/components/Input/Input";
+
+const CoursesSearch = () => {
+  return (
+    <div className="flex items-center justify-between gap-5 mb-[32px]">
+      <h3 className="text-[32px] font-semibold">Tất cả khóa học</h3>
+      <div className="relative">
+        <Input
+          name="search"
+          content="Tìm kiếm"
+          style="pr-16"
+          icon={
+            <Search
+              size={20}
+              className="absolute right-4 top-[50%] transform translate-y-[-50%]"
+            />
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CoursesSearch;
