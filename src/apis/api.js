01e21edd875f34da4b25349bda7ba93ec032b1ed
Pham Tuan <tuan.pham.super@gmail.com>
2025-05-21 11:36:19 +0700

    add laravel deployed backend server domain

--- src/apis/api.js
+++ src/apis/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 // "http://localhost:5174/api"
 
 const API_BASE_URL =
-  import.meta.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/api";
+  import.meta.env.REACT_APP_API_BASE_URL ||
+  "https://lms-backend-production-b51f.up.railway.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
